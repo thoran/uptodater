1f76ec72ea649726985d69734d8c470af5fb2b33
thoran <code@thoran.com>
2021-07-30 14:09:36 +1000

    0.0.0: A dry-run assuming a single repository and a bundler-based Ruby project.

---
+++ lib/Array/extract_optionsX.rb
@@ -0,0 +1,7 @@
+# Array/extract_optionsX.rb
+# Array#extract_options!
+
+# 20180804
+# 0.2.0 (The same version number as the current version of Thoran/Array/ExtractOptions.)
+
+require 'Thoran/Array/ExtractOptionsX/extract_optionsX'
